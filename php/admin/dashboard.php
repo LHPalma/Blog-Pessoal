3d5b3aebf30381ab661f00fa6287893c987d1cec
Luiz Palma <luizpalma16@gmail.com>
2024-06-19 19:38:59 -0300

    Setup conexão e dashboard

---
+++ php/admin/dashboard.php
@@ -0,0 +1,32 @@
+<?php
+@include "../components/connect.php"
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+
+    <!-- font awesome cdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+
+    <!-- link css customizado -->
+    <link rel="stylesheet" href="../../css/style.css">
+
+</head>
+
+<body>
+
+
+
+
+
+
+
+    <script src="../../js/admin_script.js"></script>
+</body>
+
+</html>
