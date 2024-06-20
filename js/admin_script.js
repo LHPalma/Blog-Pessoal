1faa9fcb50d9f9c98fcfff21a30e3217163c9cd0
Luiz Palma <luizpalma16@gmail.com>
2024-06-20 00:20:47 -0300

    Dashboard header completo

---
+++ js/admin_script.js
@@ -0,0 +1,9 @@
+let header = document.querySelector(".header");
+
+document.querySelector("#menu-btn").onclick = () => {
+    header.classList.toggle("active");
+}
+
+window.onscroll = () => {
+    header.classList.remove("active");
+}
--- php/admin/dashboard.php
+++ php/admin/dashboard.php
@@ -1,5 +1,16 @@
 <?php
-@include "../components/connect.php"
+
+include '../components/connect.php';
+
+session_start();
+
+$admin_id = $_SESSION['admin_id'];
+
+if (!isset($admin_id)) {
+    /* header('location:admin_login.php'); */
+    $admin_id = 1;
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -10,22 +21,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
 
-    <!-- font awesome cdn link -->
+    <!-- font awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
 
     <!-- link css customizado -->
-    <link rel="stylesheet" href="../../css/style.css">
+    <link rel="stylesheet" href="../../css/admin_style.css">
 
 </head>
 
 <body>
 
+    <!-- inicio header -->
+    <?php include "../admin/admin_header.php" ?>
 
 
+    <!-- fim header /-->
 
-
-
-
+    <!-- script js customizado -->
     <script src="../../js/admin_script.js"></script>
 </body>
 
